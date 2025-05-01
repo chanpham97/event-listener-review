d50f4a5beb3ef0dd457831753e3ea1782469a37a
Chan Pham <iamchanpham@gmail.com>
2025-05-01 20:26:02 +0000

    mastery template

---
+++ script.js
@@ -0,0 +1,23 @@
+// Task 1: Write the event listener such that when you click the change image button, the image changes to something else
+let imageButton = document.querySelector("#change-img")
+let image = document.querySelector("#practice-img")
+
+
+
+// Task 2: Write the event listener such that when you click the change text button, the text changes to something else
+let textButton = document.querySelector("#change-text")
+let text = document.querySelector("#text-target")
+
+
+
+// Task 3: Write the event listener such that when you click the show message button, the div's innerHTML says "Surprise!"
+let messageButton = document.querySelector("#show-message")
+let div = document.querySelector("#message-box")
+
+
+
+// Task 4: Write the event listener such that when you click the change style button, the p tag changes font color and family
+let styleButton = document.querySelector("#style-button")
+let pTag = document.querySelector("#style-target")
+
+
